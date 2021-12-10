b445758637ff96bc2e068cb544388c8d613f031c
kranthi5121 <58767050+kranthi5121@users.noreply.github.com>
2021-12-10 18:13:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "running the script....!!!
+echo"............................."
 OS=$(cat /etc/lsb-release)
-echo "The os details are..."$OS
+echo "The os details are:  $OS"
+echo "..............................."
