4dad9fb3ec0dfddf83e8cad414fbf366a310be9c
kranthi5121 <58767050+kranthi5121@users.noreply.github.com>
2022-01-04 16:10:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "running the script....!!!"
-echo "............................."
-OS=$(cat /etc/lsb-release)
-echo "The os details are:  $OS"
-echo "..............................."
+echo "running the script from SCM"
+
